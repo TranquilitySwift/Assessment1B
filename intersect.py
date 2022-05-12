83438c238507cb5e6f8bae1241468febb096b64a
TranquilitySwift <abhishek.karki2@students.vu.edu.au>
2022-05-12 20:26:54 +1000

    Changed the data type from int to float

--- intersect.py
+++ intersect.py
@@ -1,5 +1,5 @@
-group_One= [int(a) for a in input("Enter group number 1:"). split()]
-group_Two= [int(a) for a in input("Enter group number 2:"). split()]
+group_One= [float(a) for a in input("Enter group number 1:"). split()]
+group_Two= [float(a) for a in input("Enter group number 2:"). split()]
 print("Result:")
 setGroup_One= set(group_One)
 intersection= setGroup_One.intersection(group_Two)
