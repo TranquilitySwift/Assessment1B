5a3b827070b6fc502b91fa8afe80d1991c5b2626
TranquilitySwift <abhishek.karki2@students.vu.edu.au>
2022-05-12 20:40:47 +1000

    Changed the set from group one to group two for the same output.

--- intersect.py
+++ intersect.py
@@ -1,8 +1,8 @@
 group_One= [int(a) for a in input("Enter group number 1:"). split()]
-group_Two= [int(a) for a in input("Enter group number 1:"). split()]
+group_Two= [int(a) for a in input("Enter group number 2:").split()]
 print("Result:")
-setGroup_One= set(group_One)
-intersection= setGroup_One.intersection(group_Two)
+setGroup_Two= set(group_Two)
+intersection= setGroup_Two.intersection(group_One)
 intersectlist= list(intersection)
 
 print(intersectlist)
